86ae5f19367399979557c045e29ce5215cfda6b6
__nardLee <59244491+nard-lee@users.noreply.github.com>
2024-09-06 00:52:49 +0800

    logo added

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 import BottomTabView from './components/tab/BottomTabView';
 
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	useEffect(()=>{
+		const prepare = async ()=> {
+			try {
+				await Font.loadAsync(Entypo.font);
+				await new Promise(res => setTimeout(res, 2000));
+			} catch (err) {
+				console.warn(err)
+ 			} finally {
+				setAppIsReady(true);
+			}
+		}
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(async ()=>{
+		if(appIsReady) await SplashScreen.hideAsync();
+	}, [appIsReady]);
+
+	if(!appIsReady) return null;
+
 	return (
-		<View style={{ flex: 1 }}>
+		<View style={{ flex: 1 }} onLayoutRootView={ onLayoutRootView }>
 			<StatusBar
 				animated={true}
 				backgroundColor='#ccc'
 			/>
-			<BottomTabView />
+			<BottomTabView/>
 		</View>
 	);
 }
